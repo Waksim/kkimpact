0ad01571738ff93d387c456819032e6740a02b66
Waksim <maksenro@gmail.com>
2024-08-09 18:35:35 +0300

    Добавил прототип WebApp

--- keyboards/ua.py
+++ keyboards/ua.py
@@ -1,11 +1,11 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 kb_main_ua = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="🦊 Драфты Хвост")],
         [KeyboardButton(text="Згенерувати деку"), KeyboardButton(text="Дека з Hoyolab")],
         [KeyboardButton(text="Згенерувати 2"), KeyboardButton(text="2 з Hoyolab")],
         [KeyboardButton(text="Згенерувати 10"), KeyboardButton(text="10 з Hoyolab")],
+        [KeyboardButton(text="🦊 Драфты Хвост"), KeyboardButton(text="Дек_билдер", web_app=WebAppInfo(url=f'https://waksim.github.io/kkimpact_web/'))]
     ],
     resize_keyboard=True,
     input_field_placeholder="Яку деку хочеш?"
