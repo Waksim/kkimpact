771cf1c4de99b8284eebb680e0766d7e14e76e4c
Waksim <maksenro@gmail.com>
2024-08-15 22:14:01 +0300

    Драфты-Блепа: добавил подбор

--- keyboards/ru.py
+++ keyboards/ru.py
@@ -8,6 +8,7 @@ kb_ru_main = ReplyKeyboardMarkup(
         # [KeyboardButton(text="Сгенерировать 10"), KeyboardButton(text="10 с Hoyolab")],
         # [KeyboardButton(text="🦊 Драфты Хвост"), KeyboardButton(text="Дек_билдер", web_app=WebAppInfo(url=f'https://waksim.github.io/blep-drafts/'))]
         [KeyboardButton(text="🦊 Драфты Хвост"), KeyboardButton(text="😼 Блеп-Драфты")],
+        [KeyboardButton(text="😼 Блеп-СОЛО", web_app=WebAppInfo(url=f'https://waksim.github.io/blep-drafts/'))]
     ],
     resize_keyboard=True,
     row_width=100,
