ee5272703e77a8b62216e9ba4cb64f796ab2e79a
Waksim <maksenro@gmail.com>
2024-08-28 19:50:31 +0300

    Обновление Геншина: 5.0

--- scripts/add_new_cards_photo.py
+++ scripts/add_new_cards_photo.py
@@ -57,70 +57,125 @@ def border_and_size(input_folder, output_folder, border_image_path, new_width):
             resized_image.save(output_path, format='PNG')  # Сохраняем как PNG для сохранения альфа-канала
 
 
-# ROLE карты
-border_and_size(
-    input_folder='../img/refactor/role',
-    output_folder='../img/role_cards_lowest_border',
-    border_image_path='../img/border.png',
-    new_width=151
+def resize_images(input_folder, output_folder, scale):
+    """
+    Уменьшает изображения в указанной директории на заданный коэффициент и сохраняет их в другой директории.
+
+    :param input_folder: Путь к директории с исходными изображениями.
+    :param output_folder: Путь к директории для сохранения уменьшенных изображений.
+    :param scale: Коэффициент уменьшения изображений (по умолчанию 0.5).
+    """
+    # Убедитесь, что директория для сохранения существует
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Проходим по всем файлам в директории с изображениями
+    for filename in os.listdir(input_folder):
+        # Полный путь к изображению
+        input_path = os.path.join(input_folder, filename)
+
+        # Проверяем, что это файл изображения
+        if os.path.isfile(input_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.webp')):
+            # Открываем изображение
+            with Image.open(input_path) as img:
+                # Получаем размеры изображения
+                width, height = img.size
+
+                # Уменьшаем изображение
+                new_size = (int(width * scale), int(height * scale))
+                resized_img = img.resize(new_size, Image.LANCZOS)
+
+                # Полный путь для сохранения уменьшенного изображения
+                output_path = os.path.join(output_folder, filename)
+
+                # Сохраняем уменьшенное изображение
+                resized_img.save(output_path)
+
+
+# # ROLE карты
+# border_and_size(
+#     input_folder='../img/refactor/role',
+#     output_folder='../img/role_cards_lowest_border',
+#     border_image_path='../img/border.png',
+#     new_width=151
+# )
+# print('ROLE карты добавлены!')
+#
+# # ACTION карты
+# border_and_size(
+#     input_folder='../img/refactor/action',
+#     output_folder='../img/action_cards_lowest_border',
+#     border_image_path='../img/border.png',
+#     new_width=105
+# )
+# print('ACTION карты добавлены!')
+#
+#
+# # FOR CV
+#
+# # KK_ROLE карты
+# create_templates_for_recognize(
+#     input_folder="../img/role_cards_lowest_border",
+#     output_folder="../img/assets/templates/kkimpact_roles",
+#     desired_height=230,
+#     crop_size=10
+# )
+# print('KK_ROLE карты добавлены!')
+#
+# # KK_ACTION карты
+# create_templates_for_recognize(
+#     input_folder="../img/action_cards_lowest_border",
+#     output_folder="../img/assets/templates/kkimpact_actions",
+#     desired_height=160,
+#     crop_size=8
+# )
+# print('KK_ACTION карты добавлены!')
+#
+# # ARENA_ROLE карты
+# create_templates_for_recognize(
+#     input_folder="../img/role_cards_lowest_border",
+#     output_folder="../img/assets/templates/roles_arena",
+#     desired_height=125,
+#     crop_size=8
+# )
+# print('ARENA_ROLE карты добавлены!')
+#
+# # BASE_ROLE карты
+# create_templates_for_recognize(
+#     input_folder="../img/role_cards_lowest_border",
+#     output_folder="../img/assets/templates/role",
+#     desired_height=182,
+#     crop_size=8
+# )
+# print('BASE_ROLE карты добавлены!')
+#
+# # BASE_ACTION карты
+# create_templates_for_recognize(
+#     input_folder="../img/action_cards_lowest_border",
+#     output_folder="../img/assets/templates/action",
+#     desired_height=124,
+#     crop_size=8
+# )
+# print('BASE_ACTION карты добавлены!')
+
+
+# BLEP
+
+# # BLEP_BIG карты
+# border_and_size(
+#     input_folder="../img/refactor/BLEPS",
+#     output_folder="../img/refactor/res_BLEPS_big",
+#     border_image_path='../img/border.png',
+#     new_width=482
+# )
+# print('BLEP_BIG карты добавлены!')
+
+# BLEP_RESIZE карты
+resize_images(
+    input_folder="../img/refactor/res_BLEPS_big_original",
+    output_folder="/Users/mk/PycharmProjects/gitcg-draft/public/assets/",
+    scale=0.5
 )
-print('ROLE карты добавлены!')
-
-# ACTION карты
-border_and_size(
-    input_folder='../img/refactor/action',
-    output_folder='../img/action_cards_lowest_border',
-    border_image_path='../img/border.png',
-    new_width=105
-)
-print('ACTION карты добавлены!')
-
-
-# FOR CV
-
-# KK_ROLE карты
-create_templates_for_recognize(
-    input_folder="../img/role_cards_lowest_border",
-    output_folder="../img/assets/templates/kkimpact_roles",
-    desired_height=230,
-    crop_size=10
-)
-print('KK_ROLE карты добавлены!')
-
-# KK_ACTION карты
-create_templates_for_recognize(
-    input_folder="../img/action_cards_lowest_border",
-    output_folder="../img/assets/templates/kkimpact_actions",
-    desired_height=160,
-    crop_size=8
-)
-print('KK_ACTION карты добавлены!')
-
-# ARENA_ROLE карты
-create_templates_for_recognize(
-    input_folder="../img/role_cards_lowest_border",
-    output_folder="../img/assets/templates/roles_arena",
-    desired_height=125,
-    crop_size=8
-)
-print('ARENA_ROLE карты добавлены!')
-
-# BASE_ROLE карты
-create_templates_for_recognize(
-    input_folder="../img/role_cards_lowest_border",
-    output_folder="../img/assets/templates/role",
-    desired_height=182,
-    crop_size=8
-)
-print('BASE_ROLE карты добавлены!')
-
-# BASE_ACTION карты
-create_templates_for_recognize(
-    input_folder="../img/action_cards_lowest_border",
-    output_folder="../img/assets/templates/action",
-    desired_height=124,
-    crop_size=8
-)
-print('BASE_ACTION карты добавлены!')
+print('BLEP_RESIZE карты добавлены!')
 
 print('- END SCRIPT -')
